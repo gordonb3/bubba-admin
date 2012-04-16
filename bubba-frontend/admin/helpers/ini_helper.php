4d906edc008c7f50abef923c8144670cc60c742b
Carl Fürstenberg <carl@excito.com>
2012-04-16 14:55:15 +0200

    escape strings in inifile creation and quote them (Closes a bug)

--- bubba-frontend/admin/helpers/ini_helper.php
+++ bubba-frontend/admin/helpers/ini_helper.php
@@ -1,23 +1,30 @@
 <?php
 function write_ini_file( $filename, $array )
 {
+    function enquote($key, $val) {
+        if(is_string($val)) {
+            return "$key = \"" . addslashes($val) . "\"";
+        } else {
+            return "$key = $val";
+        }
+    }
     $res = array();
     foreach( $array as $key => $val )
     {
         if( is_array($val) ) {
             $res[] = "[$key]";
-			foreach($val as $skey => $sval) {
-				if( is_array( $sval ) ) {
-					foreach($sval as $sskey => $ssval) {
-						$res[] = "  $sskey\[\] = $ssval";
-					}
-				} else {
-					$res[] = "  $skey = $sval";
-				}
-			}
-		} else {
-			$res[] = "$key = $val";
-		}
-	}
-	file_put_contents( $filename, implode("\n",$res), LOCK_EX );
+            foreach($val as $skey => $sval) {
+                if( is_array( $sval ) ) {
+                    foreach($sval as $sskey => $ssval) {
+                        $res[] = "  " . enquote("$$sskey\[\]", $$ssval);
+                    }
+                } else {
+                    $res[] = "  " . enquote("$$skey", $$sval);
+                }
+            }
+        } else {
+            $res[] = enquote($key, $val);
+        }
+    }
+    file_put_contents( $filename, implode("\n",$res), LOCK_EX );
 }
