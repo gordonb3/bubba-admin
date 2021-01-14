18ac81ef83f038d0ec364800eb38b719ed97c84b
gordonb3 <gordon@bosvangennip.nl>
2021-01-14 14:22:09 +0100

    jquery file names should not contain a version in the final installation

--- bubba-frontend/admin/index.php
+++ bubba-frontend/admin/index.php
@@ -30,35 +30,4 @@ define('APPPATH', $application_folder.'/');
 
 require_once BASEPATH.'core/CodeIgniter.php';
 
-<?php
-define('ENVIRONMENT', 'production');
-
-if (defined('ENVIRONMENT'))
-{
-	switch (ENVIRONMENT)
-	{
-		case 'development':
-			error_reporting(E_ALL);
-		break;
-
-		case 'testing':
-		case 'production':
-			error_reporting(0);
-		break;
-
-		default:
-			exit('The application environment is not set correctly.');
-	}
-}
-
-$system_path = "/opt/codeigniter/system/";
-$application_folder = "/opt/bubba/web-admin/admin";
-
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-define('BASEPATH', str_replace("\\", "/", $system_path));
-define('FCPATH', str_replace(SELF, '', __FILE__));
-define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-define('APPPATH', $application_folder.'/');
-
-require_once BASEPATH.'core/CodeIgniter.php';
-
+?>
